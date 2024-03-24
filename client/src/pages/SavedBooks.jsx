3f018fa3d1f216d7494db588db4c855276867e81
EowynStark <andwellm89@gmail.com>
2024-03-24 16:26:50 -0500

    adding apollo hooks to loginform, adjusting syntax of savedbooks to properly add hooks

--- client/src/pages/SavedBooks.jsx
+++ client/src/pages/SavedBooks.jsx
@@ -6,45 +6,30 @@ import {
   Row,
   Col
 } from 'react-bootstrap';
-import { useMutation, useQuery } from '@apollo/client';
-import {GET_ME} from '../utils/queries';
+
+import { GET_ME } from '../utils/queries';
 import { REMOVE_BOOK } from '../utils/mutations';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 
 const SavedBooks = () => {
-  const {loading, data} = useQuery(GET_ME);
+  const {loading , data} = useQuery(GET_ME);
   const [removeBook] = useMutation(REMOVE_BOOK);
   const [userData, setUserData] = useState({});
-
-  // use this to determine if `useEffect()` hook needs to run again
-  const userDataLength = Object.keys(userData).length;
-
   useEffect(() => {
-    const getUserData = async () => {
-      try {
-        const token = Auth.loggedIn() ? Auth.getToken() : null;
+ if (!loading && data) {
+    setUserData(data.me);
+  }
+  }, [loading, data]);
+  if (loading) {
+    return <h2>LOADING...</h2>;
+  }
+ if (error) {
+  console.error(error);
+  return <h2>Something went wrong...</h2>;
+}
 
-        if (!token) {
-          return false;
-        }
-
-        const response = await getMe(token);
-
-        if (!response.ok) {
-          throw new Error('something went wrong!');
-        }
-
-        const user = await response.json();
-        setUserData(user);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-
-    getUserData();
-  }, [userDataLength]);
-
+  
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
     const token = Auth.loggedIn() ? Auth.getToken() : null;
@@ -55,25 +40,15 @@ const SavedBooks = () => {
 
     try {
       await removeBook({
-        variables: { bookId }
+        variables: { bookId },
       });
-      
+
       // upon success, remove book's id from localStorage
       removeBookId(bookId);
-      setUserData((previousUserData) => ({
-        ...previousUserData,
-        savedBooks: previousUserData.savedBooks.filter((savedBook) => savedBook.bookId !== bookId),
-      }));
     } catch (err) {
       console.error(err);
     }
-  };
-
-  // if data isn't here yet, say so
-  if (!userDataLength) {
-    return <h2>LOADING...</h2>;
   }
-
   return (
     <>
       <div fluid className="text-light bg-dark p-5">
